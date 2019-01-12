e18d0fa274f27c085c317a97794ba325c5e48440
SpaceBeeGaming <space.bee.gaming@gmail.com>
2019-01-12 22:31:07 +0200

    Added unit tests for JsonStorage

--- DiscordBot4MeCore/Storage/Implementations/JsonStorage.cs
+++ DiscordBot4MeCore/Storage/Implementations/JsonStorage.cs
@@ -1,5 +1,5 @@
-﻿using System.IO;
-
+﻿using System;
+using System.IO;
 using Newtonsoft.Json;
 
 namespace DiscordBot4MeCore.Storage.Implementations
@@ -8,14 +8,21 @@ namespace DiscordBot4MeCore.Storage.Implementations
     {
         public T RestoreObject<T>(string key)
         {
-            string json = File.ReadAllText($"{key}.json");
+            string json = "";
+            if (File.Exists(key))
+            {
+                json = File.ReadAllText($"{key}.json");
+            }
             return JsonConvert.DeserializeObject<T>(json);
         }
 
         public void StoreObject(object obj, string key)
         {
             string file = $"{key}.json";
-            Directory.CreateDirectory(Path.GetDirectoryName(file));
+            if (!string.IsNullOrWhiteSpace(Path.GetDirectoryName(file)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(file));
+            }
             string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
             File.WriteAllText(file, json);
         }
---
+++ UnitTests.xUnit/TestStorage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DiscordBot4MeCore.Storage.Implementations;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace UnitTests.xUnit
+{
+    public class TestStorage
+    {
+        [Fact]
+        public void TestJsonStoragePathKey()
+        {
+            const string key = "Config/TestKey1";
+            const string dataToStore = "ABC";
+            JsonStorage storage = new JsonStorage();
+            storage.StoreObject(dataToStore, key);
+
+            string dataFromStorage = storage.RestoreObject<string>(key);
+            Assert.Equal(expected: dataToStore, actual: dataFromStorage);
+        }
+
+        [Fact]
+        public void TestJsonStorageNoPathKey()
+        {
+            const string key = "TestKey2";
+            const string dataToStore = "ABC";
+            JsonStorage storage = new JsonStorage();
+            storage.StoreObject(dataToStore, key);
+
+            string dataFromStorage = storage.RestoreObject<string>(key);
+            Assert.Equal(expected: dataToStore, actual: dataFromStorage);
+        }
+
+        [Fact]
+        public void TestJsonStorageLoadWrongKey()
+        {
+            const string key = "tesd";
+            JsonStorage storage = new JsonStorage();
+            storage.RestoreObject<string>(key);
+        }
+
+        [Fact]
+        public void TestJsonStorageStoreEmptyKey()
+        {
+            const string key = "";
+            const string dataToStore = "ABC";
+
+            JsonStorage storage = new JsonStorage();
+            storage.StoreObject(dataToStore, key);
+        }
+
+        [Fact]
+        public void TestJsonStorageStoreNullKey()
+        {
+            const string key = null;
+            const string dataToStore = "ABC";
+
+            JsonStorage storage = new JsonStorage();
+            storage.StoreObject(dataToStore, key);
+        }
+
+        //[Fact]
+        //public void TestInMemoryStorageKey()
+        //{
+
+        //}
+    }
+}
