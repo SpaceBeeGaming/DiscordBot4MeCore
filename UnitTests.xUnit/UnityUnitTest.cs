c0346bb8bae36243514376a2399926bd77c1a7a2
SpaceBeeGaming <space.bee.gaming@gmail.com>
2019-01-12 22:30:46 +0200

    Changed UnityUnit tests and removed test unit test

--- UnitTests.xUnit/UnityUnitTest.cs
+++ UnitTests.xUnit/UnityUnitTest.cs
@@ -9,25 +9,15 @@ namespace UnitTests.xUnit
     public class UnityUnitTest
     {
         [Fact]
-        public void Test1()
-        {
-            const int expected = 5;
-            int actual = MyUtility.MyTestUtility(expected);
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
         public void TestUnityNewContainer()
         {
-            IDataStorage actual = DiscordBot4MeCore.Unity.Resolve<IDataStorage>();
-            Assert.IsType<JsonStorage>(actual);
+            DiscordBot4MeCore.Unity.Resolve<IDataStorage>();
         }
 
         [Fact]
         public void TestUnityExistingContainer()
         {
-            ILogger actual = DiscordBot4MeCore.Unity.Resolve<ILogger>();
-            Assert.IsType<Logger>(actual);
+            DiscordBot4MeCore.Unity.Resolve<ILogger>();
         }
     }
 }
